2dee0e14a0cece3ffd29c61023cd0c3deac93921
Tomonori-Tobita <other.side.5695@gmail.com>
2020-05-24 05:45:42 +0900

    第3回課題(完成型)

--- qanda/config/routes.rb
+++ qanda/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-  get 'questions/show'
-  get 'questions/new'
-  get 'questions/edit'
+  get 'answers/edit'
+  root 'questions#index'
+  resources :questions do
+    resources :answers
+  end  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- qanda/db/schema.rb
+++ qanda/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_001802) do
+ActiveRecord::Schema.define(version: 2020_05_22_214145) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "name"
